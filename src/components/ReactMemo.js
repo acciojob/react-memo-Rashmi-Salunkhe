a420ac75080b221952200137c6b61cbf4d99d47e
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-18 10:58:22 +0530

    Create ReactMemo.js

---
+++ src/components/ReactMemo.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const ReactMemo = () => {
+  const[skills,setSkills] = useState([]);
+  const[skillinput,setSkillinput] = useState('');
+
+  return (
+    <div>
+        <h1>React.memo</h1>
+        <input id='skill-input' type='text' onChange={(e)=>setSkillinput(e.target.value)}  value={skillinput}/>
+        <button id='skill-btn' onClick={()=>setSkills([...skills,skillinput])}>Add Skill</button>
+        <ul id='item-jumbotron'>
+            {
+              skills.map((skill,index)=><li key={index}>{skill}</li>)
+            }
+        </ul>
+    </div>
+  )
+}
+
+export default ReactMemo
